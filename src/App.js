4d3f9d96d651997dae235138143b7fd206f174bf
Steven Choi <stevencschoi@gmail.com>
2020-05-19 18:22:32 -0400

    added stylings and nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
+import Nav from "./components/Nav";
 import Register from "./components/Register";
 import TodoForm from "./components/TodoForm";
 
@@ -8,20 +9,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <Register />
+        <Nav />
       </header>
+      <Register />
       <TodoForm />
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,11 +2,21 @@ import React from "react";
 import Button from "./Button";
 
 export default function Todo(props) {
+  const deleteTodo = i => {
+    // delete todo[i]
+    console.log("delete");
+  };
+
+  const toggleComplete = i => {
+    // todo[i].isComplete = !todo[i].isComplete
+    console.log("toggle complete");
+  };
+
   return (
     <div className="todo">
-      <input type="checkbox" name="isComplete" />
+      <input type="checkbox" name="isComplete" onChange={toggleComplete} />
       <p>{props.text}</p>
-      <Button>Delete</Button>
+      <Button onClick={deleteTodo}>Delete</Button>
     </div>
   );
 }
